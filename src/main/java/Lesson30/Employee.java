7a4f43e3c85788142352ae3febffeb666d0eb93c
Valery Blagodarov <11w-walerik@mail.ru>
2025-11-17 11:56:37 +0300

    L30 done 2

--- src/main/java/Lesson30/Employee.java
+++ src/main/java/Lesson30/Employee.java
@@ -5,12 +5,9 @@ import java.util.Objects;
 import java.util.function.Predicate;
 
 public class Employee {
-    String name = "Ivan";
-    String department = "IT";
-    int salary = 1000;
-
-    public Employee() {
-    }
+    String name;
+    String department;
+    int salary;
 
     Employee(String name, String department, int salary) {
         this.name = name;
@@ -36,24 +33,28 @@ class TestEmployee{
 
     public static void main(String[] args) {
         ArrayList<Employee> list = new ArrayList<>();
-        Employee emp1 = new Employee();
-        Employee emp2 = new Employee("Elena", "Buh", 1300);
-        Employee emp3 = new Employee("Mech", "Mech", 1600);
-        Employee emp4 = new Employee("Diana", "Med", 2500);
-        Employee emp5 = new Employee("Valery", "IT", 6500);
+        Employee emp1 = new Employee("Elena", "Buh", 1300);
+        Employee emp2 = new Employee("Mech", "Mech", 1600);
+        Employee emp3 = new Employee("Diana", "Med", 2500);
+        Employee emp4 = new Employee("Valery", "IT", 6500);
+        Employee emp5 = new Employee("Dmitry", "IT", 5100);
+        Employee emp6 = new Employee("Marina", "Buh", 3500);
+        Employee emp7 = new Employee("Petr", "Mech", 1700);
         list.add(emp1);
         list.add(emp2);
         list.add(emp3);
         list.add(emp4);
         list.add(emp5);
+        list.add(emp6);
+        list.add(emp7);
 
         TestEmployee te = new TestEmployee();
 
         System.out.println("----------IT and zp > 5000----------");
-        te.filterEmployees(list, (Employee e) -> {return Objects.equals(e.department, "IT") && e.salary > 5000;});
+        te.filterEmployees(list, (Employee e) -> Objects.equals(e.department, "IT") && e.salary > 5000);
         System.out.println("----------name startsW E and salary !=450 ----------");
         te.filterEmployees(list, e -> e.name.startsWith("E") && e.salary != 450);
         System.out.println("----------name = department----------");
-        te.filterEmployees(list, e -> {return Objects.equals(e.name, e.department);});
+        te.filterEmployees(list, e -> Objects.equals(e.name, e.department));
     }
 }
